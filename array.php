683dc94a8a05952708b1ddc8c2a3cc618785503a
name <abc@def.com>
2023-02-22 10:24:03 +0900

    배열

--- array.php
+++ array.php
@@ -1,50 +1,25 @@
 <?php
-class MyClass {
-    const CONST_VALUE = 'A constant value';
-}
- 
-$classname = 'MyClass';
-echo $classname::CONST_VALUE; // As of PHP 5.3.0
- 
-echo MyClass::CONST_VALUE;
-?>
 
-<?php
-class OtherClass extends MyClass
-{
-    public static $my_static = 'static var';
- 
-    public static function doubleColon() {
-        echo parent::CONST_VALUE . "\n";
-        echo self::$my_static . "\n";
-    }
-}
- 
-$classname = 'OtherClass';
-echo $classname::doubleColon(); // As of PHP 5.3.0
- 
-OtherClass::doubleColon();
-?>
+$arr = array( // 1차원 배열을 3개 갖는 2차원 배열 선언
+    array(),
+    array(),
+    array()
+);
 
-<?php
-class MyClass
-{
-    protected function myFunc() {
-        echo "MyClass::myFunc()\n";
-    }
-}
- 
-class OtherClass extends MyClass
-{
-    // Override parent's definition
-    public function myFunc()
-    {
-        // But still call the parent function
-        parent::myFunc();
-        echo "OtherClass::myFunc()\n";
-    }
-}
- 
-$class = new OtherClass();
-$class->myFunc();
+$arr[0][0] = "apple"; // 배열 요소 입력
+$arr[0][1] = "korea";
+$arr[0][2] = 1000;
+
+$arr[1][0] = "banana";
+$arr[1][1] = "philippines";
+$arr[1][2] = 2000;
+
+$arr[2][0] = "orange";
+$arr[2][1] = "us";
+$arr[2][2] = 1500;
+
+echo $arr[0][0]. ", " .$arr[0][1]. ", " .$arr[0][2]."\n";
+echo $arr[1][0]. ", " .$arr[1][1]. ", " .$arr[1][2]."\n";
+echo $arr[2][0]. ", " .$arr[2][1]. ", " .$arr[2][2];
+
 ?>
